cb19d2f1f40ad75b586dccdbb752b9cad2f8ee51
hftamayo <hftamayo@gmail.com>
2023-06-25 16:42:17 -0600

    adding handleCheck method for update state

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -15,10 +15,28 @@ export const Todos = () => {
     const updatedTodos = todos.filter((todo) => todo.id !== id);
     setTodos(updatedTodos);
   };
+
+  const handleCheckTodo = (id: string) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted,
+        };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
     <section>
       {todos.map((todo) => (
-        <Row key={todo.id} todo={todo} handleDeleteTodo={handleDeleteTodo} />
+        <Row
+          key={todo.id}
+          todo={todo}
+          handleDeleteTodo={handleDeleteTodo}
+          handleCheckTodo={handleCheckTodo}
+        />
       ))}
     </section>
   );
