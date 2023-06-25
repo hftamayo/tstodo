ae659331539b8cb659e4bba1bd7352ff7d612e58
hftamayo <hftamayo@gmail.com>
2023-06-24 20:28:20 -0600

    adjusting components for deleteTodo method

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import { Row } from "./Row";
 import { data } from "../todos";
+
+type Todo = {
+  id: string;
+  task: string;
+  isCompleted: boolean;
+};
+
 export const Todos = () => {
+  const [todos, setTodos] = useState<Todo[]>(data);
+
+  const handleDeleteTodo = (id: string) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
   return (
     <section>
-      {data.map((todo) => (
-        <Row key={todo.id} todo={todo} />
+      {todos.map((todo) => (
+        <Row key={todo.id} todo={todo} handleDeleteTodo={handleDeleteTodo} />
       ))}
     </section>
   );
